23b6350a1d72c7f0905d8cd7fd10c1e31345108c
Kendall Spears <1kspears23@gmail.com>
2022-04-19 14:47:16 -0500

    server and api are talking

---
+++ server/app.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const db = require('../back-end/database.js');
+const client = require('../front-end/client.js');
+
+const app = express();
+const PORT = 3000;
+
+var corsOptions = {
+    origin:'https//localhost:3000'
+} 
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+    res.json({ message: "Welcome to Ready Set Chore application." });
+    console.log("Hello World")
+})
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server! said the READY SET CHORE" });
+});
+
+app.listen(PORT, () => {
+    console.log(`from app The Server listening on ${PORT}`);
+});
+
+module.exports = app
